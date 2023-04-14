bbf6ba2d2b60b11816bb406d359ed6379544a826
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-04-14 19:08:01 -0400

    feat: update restaurant card component

--- dev/FoodForThought/src/features/restaurants/components/restaurant-info-card.component.js
+++ dev/FoodForThought/src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { SvgXml } from "react-native-svg";
 
-import star from "../../../../assets/star";
-import open from "../../../../assets/open";
+import Star from "../../../../assets/star";
+import Open from "../../../../assets/open";
 import {
   CardContainer,
   Cover,
@@ -11,9 +11,8 @@ import {
   Info,
   Rating,
   Address,
-  Icon,
+  Lodging,
 } from "./restaurant-info-card.styles";
-import { Spacer } from "../../../components/spacer/spacer.component";
 import { Typography } from "../../../components/typography/text.component";
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -39,17 +38,13 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
         <Section>
           <Rating>
             {ratingArray.map(() => (
-              <SvgXml xml={star} width={20} height={20} />
+              <SvgXml xml={Star} width="20" height="20" />
             ))}
           </Rating>
           <SectionEnd>
             {isClosedTemporarily && <Typography variant="error">CLOSED TEMPORARILY</Typography>}
-            <Spacer position="left" size="large">
-              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
-            </Spacer>
-            <Spacer position="left" size="large">
-              <Icon source={{ uri: icon }} />
-            </Spacer>
+            {isOpenNow && <SvgXml xml={Open} width="16" height="16" />}
+            <Lodging source={{ uri: icon }} />
           </SectionEnd>
         </Section>
         <Address>{address}</Address>
