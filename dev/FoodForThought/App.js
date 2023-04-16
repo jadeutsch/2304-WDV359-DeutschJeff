009f880550b14e9cd7fa6cbad356a5587e53b48d
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-04-15 23:06:33 -0400

    feat: create basic tab navigation

--- dev/FoodForThought/App.js
+++ dev/FoodForThought/App.js
@@ -3,9 +3,27 @@ import React from "react";
 import { ThemeProvider } from "styled-components/native";
 import { useFonts, Merienda_400Regular } from "@expo-google-fonts/merienda";
 import { MerriweatherSans_400Regular } from "@expo-google-fonts/merriweather-sans";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import { theme } from "./src/infrastructure/theme/index";
+import { SafeArea } from "./src/utils/safe-area.component";
 import { RestaurantScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { Text } from "react-native";
+
+// demo map and settings screens for basic navigation
+const MapScreen = () => (
+  <SafeArea>
+    <Text>Map</Text>
+  </SafeArea>
+);
+const SettingsScreen = () => (
+  <SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>
+);
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,7 +37,13 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantScreen />
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Restaurants" component={RestaurantScreen} />
+            <Tab.Screen name="Map" component={MapScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
