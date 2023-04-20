161fd2a10035403bda738c014b0a883501cbf760
jeffdeutsch <deutsch.jeff.a@gmail.com>
2023-04-20 12:52:33 -0400

    feat: add additional data points to show scrolling list

--- dev/FoodForThought/src/features/restaurants/screens/restaurants.screen.js
+++ dev/FoodForThought/src/features/restaurants/screens/restaurants.screen.js
@@ -27,7 +27,15 @@ export const RestaurantScreen = () => {
         <SearchBar placeholder="Search" elevation={2} />
       </SearchContainer>
       <RestaurantList
-        data={[{ name: 1 }, { name: 2 }]}
+        data={[
+          { name: 1 },
+          { name: 2 },
+          { name: 3 },
+          { name: 4 },
+          { name: 5 },
+          { name: 6 },
+          { name: 7 },
+        ]}
         renderItem={() => <RestaurantInfoCard />}
         keyExtractor={(item) => item.name}
       />
