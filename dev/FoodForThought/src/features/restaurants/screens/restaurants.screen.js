e3b7b0ba403cb9bacebd34d7d2da7f7ec5055f2c
jeffdeutsch <deutsch.jeff.a@gmail.com>
2023-04-20 15:59:45 -0400

    feat: add loading screen to restaurant screen

--- dev/FoodForThought/src/features/restaurants/screens/restaurants.screen.js
+++ dev/FoodForThought/src/features/restaurants/screens/restaurants.screen.js
@@ -1,11 +1,20 @@
 import React, { useContext } from "react";
 import { FlatList, View } from "react-native";
-import { Searchbar } from "react-native-paper";
+import { Searchbar, ActivityIndicator } from "react-native-paper";
 import styled from "styled-components/native";
 
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { SafeArea } from "../../../utils/safe-area.component";
+
+const LoadingContainer = styled(View)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+const Loading = styled(ActivityIndicator)`
+  margin-left: -50px;
+`;
 
 const SearchContainer = styled(View)`
   padding: ${(props) => props.theme.space[3]};
@@ -25,6 +34,11 @@ export const RestaurantScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={100} animating={true} color={"#4c6a14"} />
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <SearchBar placeholder="Search" elevation={2} />
       </SearchContainer>
