929999f0571437856fd16ed0adb57ee69991d546
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-04-23 15:14:27 -0400

    feat: create restaurant detail screen

---
+++ dev/FoodForThought/src/features/restaurants/screens/restaurant-detail.screen.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { SafeArea } from "../../../utils/safe-area.component";
+
+export const RestaurantDetailScreen = ({ route }) => {
+  const { restaurant } = route.params;
+  return (
+    <SafeArea>
+      <RestaurantInfoCard restaurant={restaurant} />
+    </SafeArea>
+  );
+};
