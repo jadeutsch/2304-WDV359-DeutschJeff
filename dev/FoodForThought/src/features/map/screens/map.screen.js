f2494e032b10b9d3f01f0cde2095f90ffe77c817
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-04-23 19:44:42 -0400

    feat: create basic map screen

---
+++ dev/FoodForThought/src/features/map/screens/map.screen.js
@@ -0,0 +1,17 @@
+import React, { useContext, useState, useEffect } from "react";
+import MapView from "react-native-maps";
+import styled from "styled-components/native";
+
+import { Search } from "../components/search.component";
+
+const Map = styled(MapView)`
+  width: 100%;
+  height: 100%;
+`;
+
+export const MapScreen = () => (
+  <>
+    <Search />
+    <Map />
+  </>
+);
