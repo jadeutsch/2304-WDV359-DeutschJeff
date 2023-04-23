92e0a3693629385ef8bfa81e1335153d5681cb57
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-04-23 00:42:43 -0400

    feat: add location context to restaurant context

--- dev/FoodForThought/src/services/restaurants/restaurants.context.js
+++ dev/FoodForThought/src/services/restaurants/restaurants.context.js
@@ -1,6 +1,7 @@
-import React, { useState, createContext, useEffect, useMemo } from "react";
+import React, { useState, createContext, useEffect, useMemo, useContext } from "react";
 
 import { restaurantsRequest, camelizeFoodData } from "./restaurants.service";
+import { LocationContext } from "../location/location.context";
 
 export const RestaurantsContext = createContext();
 
@@ -9,10 +10,14 @@ export const RestaurantsContextProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const retrieveRestaurants = () => {
+  const { location } = useContext(LocationContext);
+
+  const retrieveRestaurants = (locale) => {
     setIsLoading(true);
+    setRestaurants([]);
+
     setTimeout(() => {
-      restaurantsRequest()
+      restaurantsRequest(locale)
         .then(camelizeFoodData)
         .then((data) => {
           setIsLoading(false);
@@ -26,8 +31,11 @@ export const RestaurantsContextProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    retrieveRestaurants();
-  }, []);
+    if (location) {
+      const locationString = `${location.lat},${location.lng}`;
+      retrieveRestaurants(locationString);
+    }
+  }, [location]);
 
   return (
     <RestaurantsContext.Provider value={{ restaurants, isLoading, error }}>
