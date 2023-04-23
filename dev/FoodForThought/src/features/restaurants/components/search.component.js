c12303b7eff2c63b1698d5d2ff9cd16d4b6b45b9
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-04-23 00:39:17 -0400

    feat: create search bar component

---
+++ dev/FoodForThought/src/features/restaurants/components/search.component.js
@@ -0,0 +1,37 @@
+import React, { useContext, useState, useEffect } from "react";
+import { View } from "react-native";
+import { Searchbar } from "react-native-paper";
+import styled from "styled-components/native";
+
+import { LocationContext } from "../../../services/location/location.context";
+
+const SearchContainer = styled(View)`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const SearchBar = styled(Searchbar)`
+  background-color: ${(props) => props.theme.colors.bg.tertiary};
+`;
+
+export const Search = () => {
+  const { keyword, search } = useContext(LocationContext);
+  const [searchKeyword, setSearchKeyword] = useState(keyword);
+
+  useEffect(() => {
+    search(searchKeyword);
+  }, []);
+
+  return (
+    <SearchContainer>
+      <SearchBar
+        placeholder="Search By Location"
+        value={searchKeyword}
+        onSubmitEditing={() => search(searchKeyword)}
+        onChangeText={(text) => {
+          setSearchKeyword(text);
+        }}
+        elevation={2}
+      />
+    </SearchContainer>
+  );
+};
