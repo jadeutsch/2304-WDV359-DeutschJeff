6915d0e67f27fe7a21d48c28d09eff5c0d191865
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-04-23 21:35:17 -0400

    feat: create compact card for custom map callout

---
+++ dev/FoodForThought/src/components/restaurant/compact-restaurant-info.component.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Image, View, Platform } from "react-native";
+import { WebView } from "react-native-webview";
+import styled from "styled-components/native";
+
+import { Typography } from "../typography/text.component";
+
+const CompactImage = styled(Image)`
+  border-radius: 10px;
+  width: 120px;
+  height: 120px;
+`;
+
+const CompactWebview = styled(WebView)`
+  border-radius: 10px;
+  width: 120px;
+  height: 120px;
+`;
+
+const Item = styled(View)`
+  padding: 10px;
+  max-width: 120px;
+  align-items: center;
+`;
+
+const isAndroid = Platform.OS === "android";
+
+export const CompactRestaurantInfo = ({ restaurant }) => {
+  const PlatformImage = isAndroid ? CompactWebview : CompactImage;
+
+  return (
+    <Item>
+      <PlatformImage source={{ uri: restaurant.photos[0] }} />
+      <Typography center variant="caption" numberOfLines={3}>
+        {restaurant.name}
+      </Typography>
+    </Item>
+  );
+};
