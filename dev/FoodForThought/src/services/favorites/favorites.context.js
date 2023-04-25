a359fe0fb698326ee905d7446b955caee6559f47
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-04-24 23:40:59 -0400

    feat: add favorites context

--- dev/FoodForThought/src/services/favorites/favorites.context.js
+++ dev/FoodForThought/src/services/favorites/favorites.context.js
@@ -1,5 +1,27 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const FavoritesContext = createContext();
 
-export const FavoritesContextProvider = () => {};
+export const FavoritesContextProvider = ({ children }) => {
+  const [favorites, setFavorites] = useState([]);
+
+  const addFavorite = (restaurant) => {
+    setFavorites(...favorites, restaurant);
+  };
+  const removeFavorite = (restaurant) => {
+    const newFavorites = favorites.filter((x) => x.placeId !== restaurant.placeId);
+    setFavorites(newFavorites);
+  };
+
+  return (
+    <FavoritesContext.Provider
+      value={{
+        favorites,
+        addToFavorites: addFavorite,
+        removeFromFavorites: removeFavorite,
+      }}
+    >
+      {children}
+    </FavoritesContext.Provider>
+  );
+};
