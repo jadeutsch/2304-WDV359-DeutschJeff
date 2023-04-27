38ddb3d6fd2e88da8c03f7f701acf6ca5754f164
jeffdeutsch <deutsch.jeff.a@gmail.com>
2023-04-27 13:23:07 -0400

    fix: wrap plugins correctly

--- dev/FoodForThought/babel.config.js
+++ dev/FoodForThought/babel.config.js
@@ -3,12 +3,14 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      "module:react-native-dotenv",
-      {
-        envName: "APP_ENV",
-        moduleName: "@env",
-        path: ".env",
-      },
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: ".env",
+        },
+      ],
     ],
     env: {
       production: {
