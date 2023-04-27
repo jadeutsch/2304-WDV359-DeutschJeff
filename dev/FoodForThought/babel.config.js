ba2980283af108c38178a0d49d776fdf9fa8a28e
jeffdeutsch <deutsch.jeff.a@gmail.com>
2023-04-27 11:15:51 -0400

    feat: add env setup

--- dev/FoodForThought/babel.config.js
+++ dev/FoodForThought/babel.config.js
@@ -2,6 +2,14 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      "module:react-native-dotenv",
+      {
+        envName: "APP_ENV",
+        moduleName: "@env",
+        path: ".env",
+      },
+    ],
     env: {
       production: {
         plugins: ["react-native-paper/babel"],
