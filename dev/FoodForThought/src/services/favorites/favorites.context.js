ea7ecc72d31c4fa029cafe0fea91acb8b8929f4b
jeffdeutsch <deutsch.jeff.a@gmail.com>
2023-04-27 00:08:19 -0400

    feat: add more context to store favorites locally

--- dev/FoodForThought/src/services/favorites/favorites.context.js
+++ dev/FoodForThought/src/services/favorites/favorites.context.js
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FavoritesContext = createContext();
 
@@ -8,12 +9,41 @@ export const FavoritesContextProvider = ({ children }) => {
   const addFavorite = (restaurant) => {
     setFavorites([...favorites, restaurant]);
   };
+
   const removeFavorite = (restaurant) => {
     const newFavorites = favorites.filter(
       (x) => x.placeId !== restaurant.placeId
     );
     setFavorites(newFavorites);
   };
+
+  const saveFavorites = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem("@favorites", jsonValue);
+    } catch (err) {
+      console.error("Storage error", err);
+    }
+  };
+
+  const loadFavorites = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@favorites");
+      if (value !== null) {
+        setFavorites(JSON.parse(value));
+      }
+    } catch (err) {
+      console.error("Loading error", err);
+    }
+  };
+
+  useEffect(() => {
+    loadFavorites(favorites);
+  }, []);
+
+  useEffect(() => {
+    saveFavorites(favorites);
+  }, [favorites]);
 
   return (
     <FavoritesContext.Provider
