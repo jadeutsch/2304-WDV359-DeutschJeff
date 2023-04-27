f5b73259b0bfd2608ae5ab96d61996e21426a9dd
jeffdeutsch <deutsch.jeff.a@gmail.com>
2023-04-27 11:18:35 -0400

    feat: add firebase configuration

--- dev/FoodForThought/App.js
+++ dev/FoodForThought/App.js
@@ -3,12 +3,32 @@ import React from "react";
 import { ThemeProvider } from "styled-components/native";
 import { useFonts, Merienda_400Regular } from "@expo-google-fonts/merienda";
 import { MerriweatherSans_400Regular } from "@expo-google-fonts/merriweather-sans";
+import { initializeApp } from "firebase/app";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+} from "@env";
 
 import { theme } from "./src/infrastructure/theme/index";
 import { FavoritesContextProvider } from "./src/services/favorites/favorites.context";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { Navigation } from "./src/infrastructure/navigation";
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+};
+
+initializeApp(firebaseConfig);
 
 export default function App() {
   const [fontsLoaded] = useFonts({
