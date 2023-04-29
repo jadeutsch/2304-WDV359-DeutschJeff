3d9291bd4daebb695366e1c51bf9f4eef6524eb6
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-04-29 15:59:28 -0400

    feat: add animation to main screen

--- dev/FoodForThought/src/features/account/screens/account.screen.js
+++ dev/FoodForThought/src/features/account/screens/account.screen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import LottieView from "lottie-react-native";
 
 import {
   AccountBackground,
@@ -8,21 +9,27 @@ import {
   RegisterButton,
   MiniSpacer,
   Title,
+  AnimationWrapper,
 } from "../components/account.styles";
 
 export const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <AnimationWrapper>
+        <LottieView
+          key="animation"
+          autoPlay
+          loop
+          resizeMode="cover"
+          source={require("../../../../assets/mobilefood.json")}
+        />
+      </AnimationWrapper>
       <Title>Food For Thought</Title>
       <AccountContainer>
-        <AuthButton onPress={() => navigation.navigate("Login")}>
-          Login
-        </AuthButton>
+        <AuthButton onPress={() => navigation.navigate("Login")}>Login</AuthButton>
         <MiniSpacer />
-        <RegisterButton onPress={() => navigation.navigate("Register")}>
-          Register
-        </RegisterButton>
+        <RegisterButton onPress={() => navigation.navigate("Register")}>Register</RegisterButton>
       </AccountContainer>
     </AccountBackground>
   );
