11177b82780803e13458d24df36e18dcd0de0bee
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-04-30 17:26:42 -0400

    feat: create placesRequest function for mock data and images

---
+++ dev/FoodForThought/functions/places/index.js
@@ -0,0 +1,11 @@
+const { mocks, addMockImage } = require("./mock");
+const url = require("url");
+
+module.exports.placesRequest = (request, response) => {
+  const { location, mock } = url.parse(request.url, true).query;
+  const data = mocks[location];
+  if (data) {
+    data.results = data.results.map(addMockImage);
+  }
+  response.json(data);
+};
