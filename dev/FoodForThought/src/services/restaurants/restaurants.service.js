2406de67f95d41026c9c60662c85f5f8f76c9255
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-04-30 17:27:52 -0400

    feat: add restaurant location fetch from server

--- dev/FoodForThought/src/services/restaurants/restaurants.service.js
+++ dev/FoodForThought/src/services/restaurants/restaurants.service.js
@@ -1,22 +1,13 @@
-import { mocks, mockImages } from "./mock";
 import camelize from "camelize";
 
-// sets default location as san francisco from mock data
-export const restaurantsRequest = (location) => {
-  return new Promise((resolve, reject) => {
-    const mock = mocks[location];
-    if (!mock) {
-      reject("Not Found");
-    }
-    resolve(mock);
-  });
+export const restaurantsRequest = async (location) => {
+  return await fetch(
+    `http://127.0.0.1:5001/foodforthought-3272f/us-central1/placesNearby?location=${location}`
+  ).then((response) => response.json());
 };
 
 export const camelizeFoodData = ({ results = [] }) => {
   const mappedResults = results.map((restaurant) => {
-    restaurant.photos = restaurant.photos.map((p) => {
-      return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
-    });
     return {
       ...restaurant,
       isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
