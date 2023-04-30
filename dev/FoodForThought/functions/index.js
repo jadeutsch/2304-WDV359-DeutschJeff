a86b62694e6bc4f3184038e90ed264d58ac1cfe6
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-04-30 16:21:59 -0400

    feat: add geocode function to firebase functions

--- dev/FoodForThought/functions/index.js
+++ dev/FoodForThought/functions/index.js
@@ -1,9 +1,9 @@
 const functions = require("firebase-functions");
+const { geocodeRequest } = require("./geocode");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.geocode = functions.https.onRequest((request, response) => {
+  geocodeRequest(request, response);
+});
