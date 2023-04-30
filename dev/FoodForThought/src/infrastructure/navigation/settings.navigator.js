c47477de011a04eaafd70c43a1d0639779c4b582
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-04-30 01:46:25 -0400

    feat: create settings navigation stack

---
+++ dev/FoodForThought/src/infrastructure/navigation/settings.navigator.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { CardStyleInterpolators } from "@react-navigation/stack";
+
+import { SettingsScreen } from "../../features/settings/screens/settings.screens";
+import { FavoritesScreen } from "../../features/settings/screens/favorites.screens";
+
+const SettingsStack = createNativeStackNavigator();
+
+export const SettingsNavigator = ({ route, navigation }) => {
+  return (
+    <SettingsStack.Navigator
+      screenOptions={{
+        headerMode: "screen",
+        cardStyleInterpolators: CardStyleInterpolators.forHorizontalIOS,
+      }}
+    >
+      <SettingsStack.Screen
+        options={{ header: () => null }}
+        name="Settings"
+        component={SettingsScreen}
+      />
+      <SettingsStack.Screen name="Favorites" component={FavoritesScreen} />
+    </SettingsStack.Navigator>
+  );
+};
