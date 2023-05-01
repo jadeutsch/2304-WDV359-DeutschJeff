7b6a58c150d8a70a15618e816930d4354089dc2c
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-04-30 21:38:45 -0400

    feat: set mock data to false

--- dev/FoodForThought/src/utils/env.js
+++ dev/FoodForThought/src/utils/env.js
@@ -5,5 +5,5 @@ const localHost = "http://127.0.0.1:5001/foodforthought-3272f/us-central1";
 
 export const isAndroid = Platform.OS === "android";
 export const isDevelopment = process.env.NODE_ENV === "development";
-// export const isMock = false;
+export const isMock = false;
 export const host = !isDevelopment || isAndroid ? liveHost : localHost;
