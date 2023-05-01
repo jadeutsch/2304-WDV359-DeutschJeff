e3af6d5c8f55e7af7d731e654efb2826dce04b15
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-05-01 03:23:40 -0400

    feat: add cart context to checkout screen

--- dev/FoodForThought/src/features/checkout/screens/checkout.screens.js
+++ dev/FoodForThought/src/features/checkout/screens/checkout.screens.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Text } from "react-native";
 
 import { CreditCardInput } from "../components/credit-card.componet";
 import { SafeArea } from "../../../utils/safe-area.component";
+import { CartContext } from "../../../services/cart/cart.context";
 
 export const CheckoutScreen = () => {
+  const { cart, restaurant } = useContext(CartContext);
   return (
     <SafeArea>
+      <Text>{JSON.stringify(cart)}</Text>
+      <Text>restaurant: {JSON.stringify(restaurant)}</Text>
       <CreditCardInput />
     </SafeArea>
   );
