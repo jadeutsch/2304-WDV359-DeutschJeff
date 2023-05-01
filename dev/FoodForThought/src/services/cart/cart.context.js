dc2db548849a8c80d0ebe48e19d2a2156f829bc6
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-05-01 03:21:32 -0400

    feat: create cart context

---
+++ dev/FoodForThought/src/services/cart/cart.context.js
@@ -0,0 +1,40 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { AuthenticationContext } from "../authentication/authentication.context";
+
+export const CartContext = createContext();
+
+export const CartContextProvider = ({ children }) => {
+  const { user } = useContext(AuthenticationContext);
+  const [cart, setCart] = useState([]);
+  const [restaurant, setRestaurant] = useState(null);
+
+  const add = (item, rest) => {
+    if (!restaurant || restaurant.placeId !== rest.placeId) {
+      setRestaurant(rest);
+      setCart([item]);
+      return;
+    } else {
+      setCart([...cart, item]);
+    }
+  };
+
+  const clear = () => {
+    setCart([]);
+    setRestaurant(null);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        addToCart: add,
+        clearCart: clear,
+        restaurant,
+        cart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
