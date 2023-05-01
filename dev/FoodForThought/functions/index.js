b0517474c2f6c686225c0ba2546810d8632fe44d
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-04-30 21:45:10 -0400

    feat: add google api to firebase functions

--- dev/FoodForThought/functions/index.js
+++ dev/FoodForThought/functions/index.js
@@ -2,13 +2,14 @@ const functions = require("firebase-functions");
 const { geocodeRequest } = require("./geocode");
 const { placesRequest } = require("./places");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-//
+const { Client } = require("@googlemaps/google-maps-services-js");
+
+const client = new Client({});
+
 exports.geocode = functions.https.onRequest((request, response) => {
-  geocodeRequest(request, response);
+  geocodeRequest(request, response, client);
 });
 
 exports.placesNearby = functions.https.onRequest((request, response) => {
-  placesRequest(request, response);
+  placesRequest(request, response, client);
 });
