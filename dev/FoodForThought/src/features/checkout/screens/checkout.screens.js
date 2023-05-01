88a0e31022d1442f61c5f257664211ebbe67ea83
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-05-01 01:54:40 -0400

    feat: add credit card component to checkout screen

--- dev/FoodForThought/src/features/checkout/screens/checkout.screens.js
+++ dev/FoodForThought/src/features/checkout/screens/checkout.screens.js
@@ -1,5 +1,12 @@
 import React from "react";
 
+import { CreditCardInput } from "../components/credit-card.componet";
+import { SafeArea } from "../../../utils/safe-area.component";
+
 export const CheckoutScreen = () => {
-  return null;
+  return (
+    <SafeArea>
+      <CreditCardInput />
+    </SafeArea>
+  );
 };
