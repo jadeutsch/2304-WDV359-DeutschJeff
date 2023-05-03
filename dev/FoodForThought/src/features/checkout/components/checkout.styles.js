46dd7dd083421ec49cc907b7201b46550d6155ae
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-05-03 01:01:45 -0400

    style: add more styles to checkout screen

--- dev/FoodForThought/src/features/checkout/components/checkout.styles.js
+++ dev/FoodForThought/src/features/checkout/components/checkout.styles.js
@@ -1,6 +1,8 @@
 import { View } from "react-native";
-import { Avatar } from "react-native-paper";
+import { Avatar, Button, TextInput } from "react-native-paper";
 import styled from "styled-components/native";
+
+import { colors } from "../../../infrastructure/theme/colors";
 
 export const CartIconContainer = styled(View)`
   align-items: center;
@@ -20,4 +22,34 @@ export const Wrapper = styled(View)`
 
 export const MiniSpacer = styled(View)`
   margin-top: ${(props) => props.theme.space[3]};
+`;
+
+export const NameInput = styled(TextInput)`
+  margin: ${(props) => props.theme.space[3]};
+`;
+
+export const PayButton = styled(Button).attrs({
+  buttonColor: colors.brand.primary,
+  textColor: colors.text.inverse,
+  mode: "elevated",
+  icon: "cash-fast",
+})`
+  width: 80%;
+  align-self: center;
+  padding: ${(props) => props.theme.space[2]};
+`;
+
+export const ClearButton = styled(Button).attrs({
+  buttonColor: colors.ui.senary,
+  textColor: colors.text.primary,
+  mode: "elevated",
+  icon: "cart-off",
+})`
+  width: 80%;
+  align-self: center;
+  padding: ${(props) => props.theme.space[2]};
+`;
+
+export const LargeWrapper = styled(View)`
+  margin-top: ${(props) => props.theme.space[5]};
 `;
