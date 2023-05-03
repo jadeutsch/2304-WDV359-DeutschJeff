f1204dad0855e4f1a68c31fd387815b403a50e54
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-05-03 01:02:23 -0400

    feat: add pay and clear buttons to checkout screen

--- dev/FoodForThought/src/features/checkout/screens/checkout.screens.js
+++ dev/FoodForThought/src/features/checkout/screens/checkout.screens.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ScrollView } from "react-native";
 import { List } from "react-native-paper";
 
@@ -7,10 +7,20 @@ import { RestaurantInfoCard } from "../../restaurants/components/restaurant-info
 import { SafeArea } from "../../../utils/safe-area.component";
 import { Typography } from "../../../components/typography/text.component";
 import { CartContext } from "../../../services/cart/cart.context";
-import { CartIcon, CartIconContainer, Wrapper, MiniSpacer } from "../components/checkout.styles";
+import {
+  CartIcon,
+  CartIconContainer,
+  Wrapper,
+  MiniSpacer,
+  NameInput,
+  PayButton,
+  ClearButton,
+  LargeWrapper,
+} from "../components/checkout.styles";
 
 export const CheckoutScreen = () => {
-  const { cart, restaurant, sum } = useContext(CartContext);
+  const { cart, restaurant, clearCart, sum } = useContext(CartContext);
+  const [name, setName] = useState("");
 
   if (!cart.length || !restaurant) {
     return (
@@ -37,7 +47,19 @@ export const CheckoutScreen = () => {
           </List.Section>
           <Typography>Total: ${sum / 100}</Typography>
         </Wrapper>
-        <CreditCardInput />
+        <NameInput
+          label="Name"
+          value={name}
+          onChangeText={(text) => {
+            setName(text);
+          }}
+        />
+        <Wrapper>{name.length > 0 && <CreditCardInput name={name} />}</Wrapper>
+        <LargeWrapper>
+          <PayButton>Pay Now</PayButton>
+          <MiniSpacer />
+          <ClearButton onPress={clearCart}>Clear Cart</ClearButton>
+        </LargeWrapper>
       </ScrollView>
     </SafeArea>
   );
