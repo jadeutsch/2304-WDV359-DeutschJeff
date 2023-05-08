2d984b4ff180cdd7a898f1835ea031461391e416
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-05-08 01:56:43 -0400

    style: add divider component from react native paper package

--- dev/FoodForThought/src/features/restaurants/screens/restaurant-detail.screen.js
+++ dev/FoodForThought/src/features/restaurants/screens/restaurant-detail.screen.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import { ScrollView } from "react-native";
-import { List } from "react-native-paper";
+import { List, Divider } from "react-native-paper";
 
 import { ButtonWrapper, OrderButton } from "./restaurant-detail.styles";
 
@@ -28,11 +28,16 @@ export const RestaurantDetailScreen = ({ navigation, route }) => {
           onPress={() => setBreakfast(!breakfast)}
         >
           <List.Item title="French Toast" />
+          <Divider />
           <List.Item title="Scrambled Eggs" />
+          <Divider />
           <List.Item title="Pancakes" />
+          <Divider />
           <List.Item title="Belgian Waffle" />
+          <Divider />
           <List.Item title="Oatmeal" />
         </List.Accordion>
+        <Divider />
         <List.Accordion
           title="Lunch"
           left={(props) => <List.Icon {...props} icon="hamburger" />}
@@ -40,9 +45,13 @@ export const RestaurantDetailScreen = ({ navigation, route }) => {
           onPress={() => setLunch(!lunch)}
         >
           <List.Item title="Hamburger" />
+          <Divider />
           <List.Item title="Cheeseburger" />
+          <Divider />
           <List.Item title="Hot Dog" />
+          <Divider />
           <List.Item title="Grilled Chicken Sandwich" />
+          <Divider />
           <List.Item title="Caesar Salad" />
         </List.Accordion>
         <List.Accordion
@@ -52,9 +61,13 @@ export const RestaurantDetailScreen = ({ navigation, route }) => {
           onPress={() => setDinner(!dinner)}
         >
           <List.Item title="Prime Rib" />
+          <Divider />
           <List.Item title="Lemon Pepper Chicken" />
+          <Divider />
           <List.Item title="Cacio e Pepe" />
+          <Divider />
           <List.Item title="Smoked Salmon" />
+          <Divider />
           <List.Item title="Pork Chops" />
         </List.Accordion>
         <List.Accordion
@@ -64,17 +77,22 @@ export const RestaurantDetailScreen = ({ navigation, route }) => {
           onPress={() => setDrinks(!drinks)}
         >
           <List.Item title="Water" />
+          <Divider />
           <List.Item title="Tea" />
+          <Divider />
           <List.Item title="Coffee" />
+          <Divider />
           <List.Item title="Soda" />
+          <Divider />
           <List.Item title="Wine" />
+          <Divider />
           <List.Item title="Beer" />
         </List.Accordion>
       </ScrollView>
       <ButtonWrapper>
         <OrderButton
           onPress={() => {
-            addToCart({ item: "special", price: 1299 }, restaurant);
+            addToCart({ item: "Special", price: 1299 }, restaurant);
             navigation.navigate("Checkout");
           }}
         >
