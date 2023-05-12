f25be343f30187a7ca4fd7ed4fccd46012aebd81
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2023-05-11 21:44:57 -0400

    refactor: change cart icon to success icon

--- dev/FoodForThought/src/features/checkout/screens/checkout-success.screens.js
+++ dev/FoodForThought/src/features/checkout/screens/checkout-success.screens.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import { Typography } from "../../../components/typography/text.component";
 import { SafeArea } from "../../../utils/safe-area.component";
-import { CartIconContainer, CartIcon } from "../components/checkout.styles";
+import { CartIconContainer, SuccessIcon } from "../components/checkout.styles";
 
 export const CheckoutSuccessScreen = () => {
   return (
     <SafeArea>
       <CartIconContainer>
-        <CartIcon icon="check-bold" />
+        <SuccessIcon icon="check-bold" />
         <Typography variant="label">Success!</Typography>
       </CartIconContainer>
     </SafeArea>
